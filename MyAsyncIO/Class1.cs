65950437e1e42c7a075a954cf74a1ce3b89b00eb
Jimmy <kji0205@gmail.com>
2017-01-18 00:03:42 +0900

    upd

---
+++ MyAsyncIO/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAsyncIO
+{
+    class Class1
+    {
+        private static async Task<String> IssueClientRequestAsync(String serverName, String message)
+        {
+            using (var pipe = new NamedPipeClientStream(serverName, "PipeName", PipeDirection.InOut, PipeOptions.Asynchronous | PipeOptions.WriteThrough))
+            {
+                pipe.Connect();     // ReadMode를 설정하기 이전에 반드시 연결되어야 한다.
+                pipe.ReadMode = PipeTransmissionMode.Message;
+
+                // 서버에 비동기로 데이터를 전송한다.
+                Byte[] request = Encoding.UTF8.GetBytes(message);
+                await pipe.WriteAsync(request, 0, request.Length);
+
+                // 서버로부터 비동기로 데이터를 수신한다.
+                Byte[] response = new Byte[1000];
+                Int32 bytesRead = await pipe.ReadAsync(response, 0, request.Length);
+                return Encoding.UTF8.GetString(response, 0, bytesRead);
+            }   // 파이프를 닫는다.
+        }
+    }
+}
---
+++ MySyntax/MyEnum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySyntax
+{
+    enum City
+    {
+        Seoul,      // 0
+        Daejun,     // 1
+        Busan=5,    // 5
+        Jeju=10     // 10
+    }
+
+    class MyEnum
+    {
+        public void MyEnumMain()
+        {
+            City myCity;
+
+            // enum 타입에 값을 대입하는 방법
+            myCity = City.Seoul;
+
+            // enum을 int로 변환(Casting)하는 방법.
+            // (int)를 앞에 지정.
+            int cityValue = (int)myCity;
+
+            if (myCity==City.Seoul) // enum 값을 비교하는 방법
+            {
+                Console.WriteLine("Welcome to Seoul");
+            }
+
+        }
+    }
+}
